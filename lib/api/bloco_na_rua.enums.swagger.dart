ae559533e0736d5764a0c06aa444f268b93d73d1
Rafael Santos <rafael.snv@gmail.com>
2026-07-26 17:18:10 -0300

    feat: add swagger API client code generation

---
+++ lib/api/bloco_na_rua.enums.swagger.dart
@@ -0,0 +1,20 @@
+// coverage:ignore-file
+// ignore_for_file: type=lint
+
+import 'package:json_annotation/json_annotation.dart';
+
+enum RolesEnum {
+  @JsonValue(null)
+  swaggerGeneratedUnknown(null),
+
+  @JsonValue(0)
+  value_0(0),
+  @JsonValue(1)
+  value_1(1),
+  @JsonValue(2)
+  value_2(2);
+
+  final int? value;
+
+  const RolesEnum(this.value);
+}
